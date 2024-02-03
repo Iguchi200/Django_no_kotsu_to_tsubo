c6136e68dcb97e015ce91b6661dd76045320f7d6
Igu <nazuna_0311@yahoo.co.jp>
2024-02-03 12:24:55 +0900

    クラスベースのhelloworldurl2を追加。

--- helloworldproject/urls.py
+++ helloworldproject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from helloworldproject.views import helloworldfunc
+from helloworldproject.views import helloworldfunc, HelloWorldClass
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("helloworldurl/", helloworldfunc),
+    path("helloworldurl2/", HelloWorldClass.as_view()),
 ]
