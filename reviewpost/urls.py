b1a8fb814b677edb2b8330fb5fca9efa16cf4c9a
Igu <nazuna_0311@yahoo.co.jp>
2024-02-06 19:58:03 +0900

    レビューアプリケーションにログイン機能を実装した。

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from reviewpost.views import signupview
+from reviewpost.views import signupview, loginview
 
 urlpatterns = [
   path("signup/", signupview, name="signup"),
+  path("login/", loginview, name="login"),
 ]
