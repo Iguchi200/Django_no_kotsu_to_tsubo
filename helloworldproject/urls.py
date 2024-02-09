2c80447d13860fbad2cbcfec985a82aca85fc205
Igu <nazuna_0311@yahoo.co.jp>
2024-02-10 05:17:00 +0900

    レビューアプリケーションに詳細（detail）ページを追加した。

--- helloworldproject/urls.py
+++ helloworldproject/urls.py
@@ -10,5 +10,7 @@ urlpatterns = (
       path("blogpost/", include("blogpost.urls")),
       path("reviewpost/", include("reviewpost.urls")),
       path("", include("helloworldapp.urls")),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from reviewpost.views import signupview, loginview, listview
+from reviewpost.views import signupview, loginview, listview, detailview
 
 urlpatterns = (
   [
     path("signup/", signupview, name="signup"),
     path("login/", loginview, name="login"),
     path("list/", listview, name="list"),
+    path("detail/<int:pk>/", detailview, name="detail"),
   ]
 )
